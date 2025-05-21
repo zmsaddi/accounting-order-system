8d4a0cc8492632a5bab157784eb31220b72c96da
zmsaddi <35869249+zmsaddi@users.noreply.github.com>
2025-05-21 19:19:06 +0200

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,1 @@
+const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('Backend Running')); app.listen(10000, () => console.log('Server on port 10000'));
---
+++ frontend/src/App.js
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function App() { return <div>نظام إدارة الطلبات</div>; }
